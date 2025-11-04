389b4dbd35f263327c8d0dbea65faf0de9f9731b
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-04 18:43:45 -0300

    Login y Signup mejorados, todavia no probé si funcionan jaja por lo menos cargan

--- elmercadito/urls.py
+++ elmercadito/urls.py
@@ -5,9 +5,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('auth/', include('users.urls')),
+    path('accounts/', include('users.urls')),
+    # path('auth/', include('users.urls')),
     path('api/auth/', include('rest_framework.urls')),
     path('', home_view, name='home'),
     path('productos/', include('products.urls')),
