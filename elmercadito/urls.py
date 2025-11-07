624c62da22a0ad4cc0dba0d0d2f775a14baf1136
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-07 00:30:17 -0300

    ahora ya no crashea al sacar la url accounts, modifique la ruta del logo

--- elmercadito/urls.py
+++ elmercadito/urls.py
@@ -5,11 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('users.urls')),
     path('api/auth/', include('rest_framework.urls')),
     path('productos/', include('products.urls')),
+    path('carrito/', include('cart.urls')),
     path('scraping/', include('scraping.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
