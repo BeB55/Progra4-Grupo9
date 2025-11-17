25a33ba7ff6008abd331234deda7da93adc8f6ec
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-17 15:53:51 -0300

    Cambio config. de Statics

--- elmercadito/settings.py
+++ elmercadito/settings.py
@@ -188,16 +188,14 @@ USE_TZ = True
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # This production code might break development mode, 
 # so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` 
     # (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
     # Enable the WhiteNoise storage backend, 
     # which compresses static files to reduce disk use
